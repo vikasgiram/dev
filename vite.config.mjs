c99093c1f2f348e0a8c95d526a2b94b6590e68ad
Vikas Giram <vikigiram@gmail.com>
2025-06-30 16:22:14 +0530

    change the vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -9,5 +9,4 @@ export default defineConfig(({ mode }) => ({
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
-  base: mode === "production" ? "/dev/" : "/", // ✅ must have trailing slash
 }));
