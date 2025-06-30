8d99cc30d847b0140aeec410a91d3c6d50486e5a
Vikas Giram <vikigiram@gmail.com>
2025-06-30 17:00:25 +0530

    fexing the image path

--- src/components/AppImage.jsx
+++ src/components/AppImage.jsx
@@ -13,7 +13,7 @@ function Image({
       alt={alt}
       className={className}
       onError={(e) => {
-        e.target.src = "/assets/images/no_image.png"
+        e.target.src = "./assets/images/no_image.png"
       }}
       {...props}
     />
