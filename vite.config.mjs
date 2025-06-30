8bb02b39f214815e6efce66f4fcc2b9e55827d27
Vikas Giram <vikigiram@gmail.com>
2025-06-30 15:34:13 +0530

    change the packages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,21 +3,11 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tagger from "@dhiwise/component-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({mode}) => ({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+export default defineConfig(({ mode }) => ({
   build: {
     outDir: "build",
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
-  base: mode === "production" ? "/dev/" : "/",
-  server: {
-    port: "4028",
-    host: "0.0.0.0",
-    strictPort: true,
-    allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
-  },
-  
+  base: mode === "production" ? "/dev/" : "/", // ✅ must have trailing slash
 }));
